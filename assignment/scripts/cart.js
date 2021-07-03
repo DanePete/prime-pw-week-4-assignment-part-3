ce47620de4c3c64842cb4a40cc3a65dcd2931da8
Dane Petersen <dane.m.petersen@gmail.com>
2021-07-03 13:49:20 -0500

    completed add item function again..

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -10,12 +10,32 @@ function addItem(item) {
 }
 
 function listItems(array) {
-  
+  for (let i of array) {
+    console.log(i);
+  }
 }
 
 function empty(array) {
-
+  basket = [];
 }
 
 
-console.log(addItem('toast'));
+/**
+ * Add Item
+ * adds an item to the basket array
+ */
+console.log(`Basket is ${basket}`);
+console.log('Adding toast (expect true)', addItem('toast'));
+console.log(`Basket is now ${basket}`);
+
+/**
+ * List Item
+ * Lists out the items in the basket array
+ */
+// console.log(listItems(basket));
+
+/**
+ * Empty Basket
+ * Sets the basket array back to empty
+ */
+// console.log(empty(basket));
